fb439f4f666c50c365b306bc18d2567b3094ddab
ifs21011-itdel <archicosemb@gmail.com>
2024-09-11 21:52:41 +0700

    Add circle ci config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.parcelize)
